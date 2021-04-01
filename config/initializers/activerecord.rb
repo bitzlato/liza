53518e86562242c2b040c02e4e9e2fa7340d7251
Danil Pismenny <danil@brandymint.ru>
2021-04-01 15:41:41 +0300

    Separate mysql and postgresql scheme.rb

--- config/initializers/activerecord.rb
+++ config/initializers/activerecord.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+ENV['SCHEMA'] = Rails.root.join('db', ['schema', ActiveRecord::Base.configurations[Rails.env].symbolize_keys[:adapter], 'rb'].join('.')).to_s
 Rails.configuration.database_support_json = \
   ActiveRecord::Base.configurations[Rails.env][:support_json]
 Rails.configuration.database_adapter = \
