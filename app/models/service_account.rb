b59c6ad57afc3ecb7a5578b7768104e66e8af215
Danil Pismenny <danil@brandymint.ru>
2021-04-06 14:25:52 +0300

    Add model ServiceAccount

---
+++ app/models/service_account.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+# ServiceAccount model
+class ServiceAccount < ApplicationRecord
+  UID_PREFIX = 'SI'
+
+  belongs_to :user, foreign_key: "owner_id", optional: true
+  has_many :api_keys, as: :key_holder_account, dependent: :destroy, class_name: 'APIKey'
+
+  scope :active, -> { where(state: 'active') }
+
+  def active?
+    self.state == 'active'
+  end
+
+  private
+
+  def update_state
+    !state_changed?
+  end
+end
