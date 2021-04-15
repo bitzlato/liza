94f504a9dcbca5d8829b0e5eef33feffdff93791
Danil Pismenny <danil@brandymint.ru>
2021-04-15 11:04:46 +0300

    Add datetime picker

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,9 +1,15 @@
 //= require rails-ujs
 //= require turbolinks
-//= require better-dom
-//= require better-dateinput-polyfill/dist/better-dateinput-polyfill.min.js
+//  require better-dom
+//  require better-dateinput-polyfill/dist/better-dateinput-polyfill.min.js
+//= require moment
+//= require moment-timezone-with-data
+//= require bootstrap4-datetimepicker/build/js/bootstrap-datetimepicker.min
 //= require_tree ./elements
 //
 document.addEventListener("turbolinks:load", function() {
   $('[data-toggle="tooltip"]').tooltip()
+  $('.datetimepicker').datetimepicker({
+     format: 'YYYY-MM-DD HH:mm'
+  })
 })
