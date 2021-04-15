1472c041b1b6359e7873d636ae656ed8236d0300
Danil Pismenny <danil@brandymint.ru>
2021-04-15 12:04:24 +0300

    Add summary

--- app/controllers/concerns/ransack_support.rb
+++ app/controllers/concerns/ransack_support.rb
@@ -4,6 +4,14 @@ module RansackSupport
     helper_method :q
   end
 
+  SUMMARY_MODELS = {
+    Deposit => { grouped_by: [:currency_id], aggregations: ['sum(amount)']},
+    Withdraw => { grouped_by: [:currency_id], aggregations: ['sum(amount)']},
+    Account => { grouped_by: [:currency_id], aggregations: ['sum(balance)','sum(locked)']},
+    Operations::Liability => { grouped_by: [:currency_id, 'operations_accounts.description'], aggregations: ['sum(debit)','sum(credit)'] },
+    Operations::Revenue => { grouped_by: [:currency_id], aggregations: ['sum(debit)', 'sum(credit)'] }
+  }
+
   def index
     render locals: {
       records: records,
@@ -15,7 +23,14 @@ module RansackSupport
   private
 
   def summary
-     records.group(:currency_id).reorder('').pluck('currency_id, sum(amount)')
+    return unless SUMMARY_MODELS[model_class].present?
+    meta = SUMMARY_MODELS[model_class]
+
+    {
+      grouped_by: meta[:grouped_by],
+      aggregations: meta[:aggregations],
+      rows: records.group(*meta[:grouped_by]).reorder('').pluck((meta[:grouped_by]+meta[:aggregations]).join(', '))
+    }
   end
 
   def q
@@ -29,6 +44,6 @@ module RansackSupport
   end
 
   def records
-    q.result.includes(model_class.reflections.select { |k, r| r.is_a? ActiveRecord::Reflection::BelongsToReflection }.keys)
+    q.result.includes(model_class.reflections.select { |k, r| r.is_a?(ActiveRecord::Reflection::BelongsToReflection) && !r.options[:polymorphic] }.keys)
   end
 end
