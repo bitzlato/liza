0ef622101e212029e0e0bace7c114e88a63d4afc
Danil Pismenny <danil@brandymint.ru>
2021-04-16 14:53:30 +0300

    Deposit and withdrawals report

--- app/models/wd_report.rb
+++ app/models/wd_report.rb
@@ -1,22 +1,44 @@
 class WdReport < Report
   class Generator < BaseGenerator
+    def initialize(form)
+      super form
+      @currencies = Set.new
+      @rows = {}
+      @keys = Set.new
+    end
     def perform
-      q = { created_at_gt: form.time_from, created_at_lteq: form.time_to }
-      deposits = SummaryQuery.new.summary(Deposit.ransack(q).result)
-      withdraws = SummaryQuery.new.summary(Withdraw.ransack(q).result)
-      currencies = Set.new
-      rows = {}
-      deposits[:rows].each do |row|
-        currencies << row.first
-        rows[row.first] ||= {}
-        rows[row.first][:deposited] = row.second
+      add_rows build_query(Deposit), :deposits
+      add_rows build_query(Withdraw), :withdraws
+      add_rows operations(Deposit, Operations::Asset), :deposit_to_operations_assets
+      add_rows operations(Withdraw, Operations::Asset), :withdraws_to_operations_assets
+      add_rows operations(Deposit, Operations::Liability), :deposit_to_operations_liabilities
+      add_rows operations(Withdraw, Operations::Liability), :withdraws_to_operations_liabilities
+      return { rows: @rows, currencies: @currencies, keys: @keys }
+      return { rows: @rows, currencies: @currencies, keys: @keys }
+    end
+
+    private
+
+    def add_rows(records, key)
+      @keys << key
+      records.each do |record|
+        currency_id, amount = record
+        @currencies << currency_id
+        @rows[currency_id] ||= {}
+        @rows[currency_id][key] = amount
       end
-      deposits[:rows].each do |row|
-        currencies << row.first
-        rows[row.first] ||= {}
-        rows[row.first][:withdrawn] = row.second
-      end
-      return { rows: rows, currencies: currencies }
+    end
+
+    def q
+      { completed: true, created_at_gt: form.time_from, created_at_lteq: form.time_to }
+    end
+
+    def operations(reference_type, klass)
+      klass.ransack(q).result.where(reference_type: reference_type.name).group(:currency_id).pluck(:currency_id, Arel.sql('sum(credit)-sum(debit)'))
+    end
+
+    def build_query(scope)
+      scope.ransack(q).result.group(:currency_id).sum(:amount)
     end
   end
 end
