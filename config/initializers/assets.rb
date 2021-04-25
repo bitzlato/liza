a837af73a4ea144ae3efc1e33ab7965bd63b8456
Danil Pismenny <danil@brandymint.ru>
2021-04-25 09:24:24 +0300

    Precompile logo.png

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -16,4 +16,4 @@ Rails.application.config.assets.prefix = "#{Settings.root_prefix}/assets"
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-Rails.application.config.assets.precompile += %w[application.js application.css]
+Rails.application.config.assets.precompile += %w[application.js application.css logo.png]
