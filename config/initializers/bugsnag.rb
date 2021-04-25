274969d504c1035f23685c36df4196cea4520734
Danil Pismenny <danil@brandymint.ru>
2021-04-25 09:40:42 +0300

    Enable staging for bugsnag

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -1,5 +1,7 @@
 Bugsnag.configure do |config|
   config.app_version = AppVersion.format('%M.%m.%p') # rubocop:disable Style/FormatStringToken
+  config.ignore_classes << HumanizedError
+  config.send_code = true
   config.send_environment = true
-  config.notify_release_stages = %w[production]
+  config.notify_release_stages = %w[production staging]
 end
