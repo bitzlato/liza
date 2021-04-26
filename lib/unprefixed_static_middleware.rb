e7e8fd24b8d289b3102bc67ffc089bcdf9639ee6
Danil Pismenny <danil@brandymint.ru>
2021-04-26 16:40:13 +0300

    Add /assets/ to unprefix middleware

--- lib/unprefixed_static_middleware.rb
+++ lib/unprefixed_static_middleware.rb
@@ -5,7 +5,10 @@ class UnprefixedStaticMiddleware
 
   def call(env)
     %w(PATH_INFO REQUEST_URI REQUEST_PATH).each do |key|
-      env[key].gsub!(Settings.root_prefix + '/uploads/', '/uploads/') if env[key].present?
+      next if env[key].blank?
+      env[key] = env[key]
+        .gsub(Settings.root_prefix + '/uploads/', '/uploads/')
+        .gsub(Settings.root_prefix + '/assets/', '/assets/')
     end
     @app.call(env)
   end
