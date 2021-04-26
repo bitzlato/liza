6126527a4d8b90168cc9b27e5041281625e6d467
Danil Pismenny <danil@brandymint.ru>
2021-04-26 16:51:32 +0300

    Customize sidekiq logger

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -4,6 +4,8 @@ require 'sidekiq'
 Sidekiq.default_worker_options = { 'backtrace' => true }
 
 if Rails.env.development? || Rails.env.production? || Rails.env.staging? || ENV['SIDEKIQ_ASYNC']
+
+  Sidekiq.logger = ActiveSupport::Logger.new Rails.root.join './log/sidekiq.log'
   Sidekiq.configure_server do |config|
     config.redis =  { url: ENV.fetch('LIZA_SIDEKIQ_REDIS_URL', 'redis://localhost:6379/3') }
     config.error_handlers << proc do |ex, context|
