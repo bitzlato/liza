02ff0d0ccece86692c03f525d4bc603c37c85c0b
Danil Pismenny <danil@brandymint.ru>
2021-05-11 16:23:37 +0300

    Add bugsnag logger

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -4,4 +4,7 @@ Bugsnag.configure do |config|
   config.send_code = true
   config.send_environment = true
   config.notify_release_stages = %w[production staging]
+  config.release_stage = Rails.env
 end
+
+Rails.logger.info "Bugsnag API key: #{Bugsnag.configuration.api_key}"
