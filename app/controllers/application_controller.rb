df3351f27d55fb64ce2db5b379331ebd039958b6
Danil Pismenny <danil@brandymint.ru>
2021-07-06 14:00:23 +0300

    Add system balances to audit page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,5 +10,11 @@ class ApplicationController < ActionController::Base
 
   skip_before_action :verify_authenticity_token if Rails.env.development?
 
+  helper_method :system_balances
+
   layout 'application'
+
+  def system_balances
+    @system_balances ||= Wallet.active.each_with_object({}) { |w, a| w.balance.each_pair { |c, b| a[c] ||= 0.0; a[c] += b.to_d } }
+  end
 end
