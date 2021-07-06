22bb5d2b1d85cbdcbb41a864e4645f2d5f502ac7
Danil Pismenny <danil@brandymint.ru>
2021-07-06 16:02:24 +0300

    Add sidekiq cron to web

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@
 # frozen_string_literal: true
 
 require 'sidekiq/web'
+require 'sidekiq/cron/web'
 
 Rails.application.routes.draw do
   default_url_options Settings.default_url_options.symbolize_keys
