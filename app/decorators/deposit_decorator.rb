b01de0946142b5ae9f8d903f8026ccdacef6a23b
Danil Pismenny <danil@brandymint.ru>
2021-07-27 12:47:10 +0300

    Add intention_id to deposit decoration

--- app/decorators/deposit_decorator.rb
+++ app/decorators/deposit_decorator.rb
@@ -6,7 +6,7 @@ class DepositDecorator < ApplicationDecorator
   delegate_all
 
   def self.table_columns
-    %i[id member created_at updated_at currency aasm_state amount fee type txid txout tid transfer_type address data]
+    %i[id member created_at updated_at currency aasm_state amount fee type txid txout tid transfer_type address intention_id data]
   end
 
   # Define presentation-specific methods here. Helpers are accessed through
