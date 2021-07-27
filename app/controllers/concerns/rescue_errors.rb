95573ab9cf865ad855bec715c0c6dbe59e435038
Danil Pismenny <danil@brandymint.ru>
2021-07-27 15:50:47 +0300

    Fix rescue error format

--- app/controllers/concerns/rescue_errors.rb
+++ app/controllers/concerns/rescue_errors.rb
@@ -8,27 +8,28 @@ module RescueErrors
   included do
     # rescue_from ActionController::UnknownFormat,     with: :rescue_unknown_format
     # rescue_from Authority::SecurityViolation,        with: :rescue_security_violation
+    # rescue_from NotAuthenticated,                    with: :not_authenticated
+    # rescue_from ActionController::InvalidAuthenticityToken, with: :rescue_invalid_authenticity_token
+
     rescue_from ActionController::MissingFile,       with: :not_found
     rescue_from ActiveRecord::RecordNotFound,        with: :not_found
-    # rescue_from NotAuthenticated,                    with: :not_authenticated
     rescue_from HumanizedError,                      with: :handle_humanized_error
-    # rescue_from ActionController::InvalidAuthenticityToken, with: :rescue_invalid_authenticity_token
   end
 
   private
 
   def handle_humanized_error(exception)
-    render 'exception', locals: { exception: exception }, layout: 'simple'
+    render 'exception', locals: { exception: exception }, layout: 'simple', formats: [:html]
   end
 
   def rescue_error(exception)
-    render 'exception', locals: { exception: exception }, layout: 'simple'
+    render 'exception', locals: { exception: exception }, layout: 'simple', formats: [:html]
   end
 
   def rescue_security_violation(exception)
     return render_json_exception exception, status: :forbidden if request.format.json?
 
-    render 'not_authorized', locals: { exception: exception }, layout: 'simple', status: :forbidden
+    render 'not_authorized', locals: { exception: exception }, layout: 'simple', status: :forbidden, formats: [:html]
   end
 
   def rescue_invalid_authenticity_token(_exception)
