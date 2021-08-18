94ca670fdab9d4306928a945f47be6b32ad4c8a1
Danil Pismenny <danil@brandymint.ru>
2021-08-18 19:18:59 +0300

    m

--- app/decorators/wallet_decorator.rb
+++ app/decorators/wallet_decorator.rb
@@ -6,6 +6,10 @@ class WalletDecorator < ApplicationDecorator
   delegate_all
 
   def self.table_columns
-    %i[id updated_at name status kind address available_balances balance_updated_at enable_invoice address_url]
+    %i[id updated_at name status kind address available_balances balance_updated_at enable_invoice]
+  end
+
+  def address
+    [h.link_to(object.address, object.address_url, target: '_blank'), present_owner(address_owner(object.address))].join('<br>').html_safe
   end
 end
