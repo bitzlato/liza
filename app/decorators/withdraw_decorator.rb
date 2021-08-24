9837230b23357a1e4fae99d615c5586424f45999
Danil Pismenny <danil@brandymint.ru>
2021-08-24 14:11:57 +0300

    Add withdraw#tx_dump

--- app/decorators/withdraw_decorator.rb
+++ app/decorators/withdraw_decorator.rb
@@ -6,7 +6,11 @@ class WithdrawDecorator < ApplicationDecorator
   delegate_all
 
   def self.table_columns
-    %i[id member created_at updated_at currency aasm_state sum amount fee type txid tid rid note beneficiary transfer_type error note]
+    %i[id member created_at updated_at currency aasm_state sum amount fee type txid tid rid note beneficiary transfer_type error note tx_dump]
+  end
+
+  def tx_dump
+    h.content_tag :span, object.tx_dump.as_json, class: 'text-small text-muted text-monospace'
   end
 
   def sum
