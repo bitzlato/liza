3a4c80ca342ca7820384b12b11b6b6066e276ab7
Danil Pismenny <danil@brandymint.ru>
2021-08-24 16:40:03 +0300

    Improve transactions form

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -6,8 +6,13 @@ class TransactionsController < ResourcesController
   include CurrencySupport
   include BlockchainSupport
   layout 'fluid'
+  helper_method :kind
 
   private
+
+  def kind
+    params.dig(:q, :kind_eq)
+  end
 
   def default_sort
     'block_number desc'
