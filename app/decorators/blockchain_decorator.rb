05d772121331668c957f4495fe1869bbde37385f
Danil Pismenny <danil@brandymint.ru>
2021-08-30 09:26:53 +0300

    Add client_options to blockchain

--- app/decorators/blockchain_decorator.rb
+++ app/decorators/blockchain_decorator.rb
@@ -6,6 +6,10 @@ class BlockchainDecorator < ApplicationDecorator
   delegate_all
 
   def self.table_columns
-    %i[key name height min_confirmations status explorer_transaction explorer_address]
+    %i[key name height min_confirmations status explorer_transaction explorer_address client_options]
+  end
+
+  def client_options
+    h.content_tag :span, object.client_options.as_json, class: 'text-small text-muted text-monospace'
   end
 end
