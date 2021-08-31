5d06b0d98d53b059ecb3f5d90026ce7e1f0ca20e
Danil Pismenny <danil@brandymint.ru>
2021-08-31 08:16:02 +0300

    Refactor navbar menu

---
+++ app/controllers/markets_controller.rb
@@ -0,0 +1,7 @@
+# Copyright (c) 2019 Danil Pismenny <danil@brandymint.ru>
+
+# frozen_string_literal: true
+
+class MraketsController < ResourcesController
+  layout 'fluid'
+end
--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
   resources :transactions
   resources :accounts
   resources :trades
+  resources :markets
   resources :orders
   resources :members
   resources :adjustments
