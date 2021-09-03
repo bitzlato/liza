b87614eac1517ef8448a72268f74dd8b5f98329e
Danil Pismenny <danil@brandymint.ru>
2021-09-03 12:15:27 +0300

    Add deposit to Serviceinvoice

--- app/decorators/service_invoice_decorator.rb
+++ app/decorators/service_invoice_decorator.rb
@@ -4,4 +4,17 @@
 
 class ServiceInvoiceDecorator < ApplicationDecorator
   delegate_all
+
+  def self.table_columns
+    super + %i[deposit]
+  end
+
+  def deposit
+    return h.middot if object.deposit.nil?
+    h.link_to h.present_deposit(object.deposit), h.deposit_path(object.deposit.id)
+  end
+
+  def invoice_id
+    h.link_to object.invoice_id, h.service_invoices_path(q: { invoice_id_eq: object.invoice_id })
+  end
 end
--- app/models/service_invoice.rb
+++ app/models/service_invoice.rb
@@ -3,4 +3,16 @@
 # Copyright (c) 2019 Danil Pismenny <danil@brandymint.ru>
 
 class ServiceInvoice < ReportsRecord
+  def deposit
+    @deposit ||= Deposit.find_by(invoice_id: deposit_invoice_id)
+  end
+
+  def wallet
+    @wallet ||= Wallet.find wallet_id
+  end
+
+  def deposit_invoice_id
+    uid = ENV.fetch('BITZLATO_API_CLIENT_UID', wallet.fetch(:uid)).to_i
+    [uid, invoice_id].join(':')
+  end
 end
