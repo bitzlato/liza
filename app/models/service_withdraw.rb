78850c79a497874ca480da233ad1e29ad561e77b
Danil Pismenny <danil@brandymint.ru>
2021-09-04 20:40:08 +0300

    Fix withdraw finder for ServiceWithdraw

--- app/models/service_withdraw.rb
+++ app/models/service_withdraw.rb
@@ -4,6 +4,6 @@
 
 class ServiceWithdraw < ReportsRecord
   def withdraw
-    @withdraw ||= Withdraw.find_by(id: withdraw_id)
+    @withdraw ||= withdraw_id.to_s.start_with?('TID') ? Withdraw.find_by(tid: withdraw_id) : Withdraw.find_by(id: withdraw_id)
   end
 end
