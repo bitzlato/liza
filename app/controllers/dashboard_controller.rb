a392ac0426605174b379cf9bc11c355e36e41644
Danil Pismenny <danil@brandymint.ru>
2021-09-06 22:34:01 +0300

    Account adjustments

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -6,7 +6,8 @@ class DashboardController < ResourcesController
   layout 'fluid'
   def index
     render locals: {
-      accountable_fee: Transaction.accountable_fee.group(:fee_currency_id).sum(:fee)
+      accountable_fee: Transaction.accountable_fee.group(:fee_currency_id).sum(:fee),
+      adjustments: Adjustment.accepted.group(:currency_id).sum(:amount)
     }
   end
 end
