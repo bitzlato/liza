a14f03a33d0d1d3ae920989d1455fc718d0ff126
Danil Pismenny <danil@brandymint.ru>
2021-09-06 18:48:35 +0300

    minor

--- app/decorators/block_number_decorator.rb
+++ app/decorators/block_number_decorator.rb
@@ -2,6 +2,6 @@ class BlockNumberDecorator < ApplicationDecorator
   delegate_all
 
   def self.table_columns
-    %i[blockchain_id number status created_at updated_at]
+    %i[blockchain_id number status transactions_processed_count created_at updated_at]
   end
 end
