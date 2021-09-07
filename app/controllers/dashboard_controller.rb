5c0acedd771e38e3566747a21749a7e03e513dca
Danil Pismenny <danil@brandymint.ru>
2021-09-07 17:29:12 +0300

    hotfix

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -6,7 +6,7 @@ class DashboardController < ResourcesController
   layout 'fluid'
   def index
     render locals: {
-      system_balances: Wallet.each_with_object({}) { |w, a| w.available_balances.each_pair { |c, b| a[c] ||= 0.0; a[c] += b.to_d } }, # rubocop:disable Style/Semicolon
+      system_balances: Wallet.all.each_with_object({}) { |w, a| w.available_balances.each_pair { |c, b| a[c] ||= 0.0; a[c] += b.to_d } }, # rubocop:disable Style/Semicolon
       accountable_fee: Transaction.accountable_fee.group(:fee_currency_id).sum(:fee),
       adjustments: Adjustment.accepted.group(:currency_id).sum(:amount)
     }
