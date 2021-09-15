a61260e9fc8a5557a62d143c584e7c20368432e5
Danil Pismenny <danil@brandymint.ru>
2021-09-15 21:19:14 +0300

    Link to transactions from block_number

--- app/decorators/block_number_decorator.rb
+++ app/decorators/block_number_decorator.rb
@@ -4,4 +4,9 @@ class BlockNumberDecorator < ApplicationDecorator
   def self.table_columns
     %i[blockchain_id number status transactions_processed_count created_at updated_at]
   end
+
+  def transactions_processed_count
+    h.link_to object.transactions_processed_count,
+      transactions_path(q: { blockchain_id_eq: object.blockchain_id, block_number_eq: object.number })
+  end
 end
