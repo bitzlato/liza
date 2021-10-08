54b2d4ce1ae6a11fc020005fb8e29327ef7f3a46
Danil Pismenny <danil@brandymint.ru>
2021-10-08 09:26:16 +0300

    Minor improve AccountLocksChecker

--- app/services/account_locks_checker.rb
+++ app/services/account_locks_checker.rb
@@ -9,6 +9,7 @@ class AccountLocksChecker
 
   def perform
     Account.transaction do
+      @account.reload
       locked_in_ask_orders = @account.ask_orders.active.sum(:locked)
       locked_in_bid_orders = @account.bid_orders.active.sum(:locked)
       locked_in_withdraws = @account.locked_in_withdraws
