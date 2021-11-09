415d96b927b6b21513a8426331e3dbef1d71ce48
Eugene Stepanov <antynitr@gmail.com>
2021-11-09 18:05:25 +0300

    Issie #16. Run Daily stat worker every day at 6 am

---
+++ app/workers/daily_stat_worker.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+#
+class DailyStatWorker
+  include Sidekiq::Worker
+
+  sidekiq_options queue: :reports
+
+  def perform
+    StatsMailer.daily(Date.yesterday).deliver_now
+  end
+end
