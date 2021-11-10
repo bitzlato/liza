53219c1a5efd71b52216173b8bc7123b1f8b1da3
Eugene. S <antynitr@gmail.com>
2021-11-10 16:50:55 +0300

    [#16] daily stats

---
+++ app/mailers/stats_mailer.rb
@@ -0,0 +1,19 @@
+class StatsMailer < ApplicationMailer
+  def daily(date = Date.current)
+    emails = ENV.fetch('DAILY_STATS_EMAILS')
+
+    # Member stat
+    @new_users_count    = Member.where(created_at: date.all_day).count
+
+    # Markets stat
+    @markets            = Market.all
+    @total_trades_count  = Trade.where(created_at: date.all_day).group(:market_id).count
+
+    # Currency stat
+    @currencies         = Currency.all
+    @total_deposit      = Deposit.success.where(created_at: date.all_day).group(:currency_id).sum(:amount)
+    @total_withdraw     = Withdraw.success.where(created_at: date.all_day).group(:currency_id).sum(:amount)
+
+    mail(to: emails, subject: "liza daily stats: #{date.to_s}")
+  end
+end
