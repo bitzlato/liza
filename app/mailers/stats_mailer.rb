7b87057ca7ee40cb30569985b87a6c7ef02680da
Eugene Stepanov <antynitr@gmail.com>
2021-11-22 10:16:56 +0300

    added weekly report

--- app/mailers/stats_mailer.rb
+++ app/mailers/stats_mailer.rb
@@ -25,4 +25,31 @@ class StatsMailer < ApplicationMailer
       subject: "Суточная статстика по бирже за #{@date}"
     )
   end
+
+  def weekly(date: Date.current, emails: ENV.fetch('DAILY_STATS_EMAILS'))
+    # Member stat
+    @new_users_count    = Member.where(created_at: date.all_week).count
+
+    # Markets stat
+    @markets            = Market.all
+
+    trade_scope = Trade.where(created_at: date.all_week).group(:market_id)
+    @total_trades_count = trade_scope.count
+    @total_trade_users_count = trade_scope.user_trades.count
+    @total_trade_bots_count = trade_scope.bot_trades.count
+
+    # Currency stat
+    @currencies         = Currency.all
+    @total_deposit      = Deposit.success.where(created_at: date.all_week).group(:currency_id).sum(:amount)
+    @total_withdraw     = Withdraw.success.where(created_at: date.all_week).group(:currency_id).sum(:amount)
+
+    @start_date         = I18n.l(date, format: '%d %B %Y (%A)')
+    @end_date           = I18n.l(date.end_of_week, format: '%d %B %Y (%A)')
+
+    mail(
+      to: emails,
+      from: ENV['SMTP_FROM'],
+      subject: "Недельная статстика по бирже c #{@start_date} по #{@end_date}"
+    )
+  end
 end
