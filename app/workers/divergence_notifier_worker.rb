c6f68e7e8281fa66ec48261500e5d2892d374905
Eugene Stepanov <antynitr@gmail.com>
2021-12-23 13:47:44 +0300

    fix app/workers/divergence_notifier_worker.rb

--- app/workers/divergence_notifier_worker.rb
+++ app/workers/divergence_notifier_worker.rb
@@ -8,7 +8,11 @@ class DivergenceNotifierWorker
   def perform
     app = ActionDispatch::Integration::Session.new(Rails.application)
     url = Rails.application.routes.url_helpers.url_for(controller: :dashboard, action: :index)
-    app.get(url)
+    authorization = if Rails.env.production?
+                      credentials = Rails.application.credentials.dig(:http_basic_auth)
+                      authorization = ActionController::HttpAuthentication::Basic.encode_credentials(credentials[:name], credentials[:password])
+                    end
+    app.process :get, url, params: {}, headers: { 'Authorization' => authorization }
     body = app.response.body
 
     if body =~ /data-divergence="true"/
