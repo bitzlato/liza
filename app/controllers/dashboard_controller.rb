ea801ac5f5589153c5aa80ea849c8d5f144a5e48
Eugene Stepanov <antynitr@gmail.com>
2021-12-27 10:25:08 +0300

    Count investments and asset_registrations for stats

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -8,7 +8,7 @@ class DashboardController < ResourcesController
     render locals: {
       system_balances: Wallet.all.each_with_object({}) { |w, a| w.available_balances.each_pair { |c, b| a[c] ||= 0.0; a[c] += b.to_d } }, # rubocop:disable Style/Semicolon
       accountable_fee: Transaction.accountable_fee.group(:fee_currency_id).sum(:fee),
-      adjustments: Adjustment.accepted.group(:currency_id).sum(:amount)
+      adjustments: Adjustment.accepted.group(:currency_id, :category).sum(:amount)
     }
   end
 end
