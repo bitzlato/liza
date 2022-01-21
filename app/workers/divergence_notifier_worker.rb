776a04e9b2f1ca8ff81388167e299076617ee278
Danil Pismenny <dapi@users.noreply.github.com>
2022-01-21 11:16:44 +0300

    Prevent divergence flood

--- app/workers/divergence_notifier_worker.rb
+++ app/workers/divergence_notifier_worker.rb
@@ -1,11 +1,31 @@
 # frozen_string_literal: true
-#
+
+require 'fileutils'
+
 class DivergenceNotifierWorker
   include Sidekiq::Worker
+  
+  STATUS_FILE = Rails.root.join('./tmp/divergence_exists')
 
   sidekiq_options queue: :reports
 
   def perform
+    return if current_status == saved_status
+    if current_status
+      SlackNotifier.notifications.ping(":warning: Обнаружены расхождения #{url}")
+    else
+      SlackNotifier.notifications.ping(":white_check_mark: Расхождения устранены #{url}")
+    end
+    save_status! current_status
+  end
+
+  private
+  
+  def current_status
+    @current_status ||= find_current_status
+  end
+  
+  def find_current_status
     app = ActionDispatch::Integration::Session.new(Rails.application)
     url = Rails.application.routes.url_helpers.url_for(controller: :dashboard, action: :index)
     authorization = if Rails.env.production?
@@ -14,10 +34,18 @@ class DivergenceNotifierWorker
                     end
     app.process :get, url, params: {}, headers: { 'Authorization' => authorization }
     body = app.response.body
+    body =~ /data-divergence="true"/
+  end
 
-    if body =~ /data-divergence="true"/
-      SlackNotifier.notifications.ping("Обнаружены расхождения #{url}")
+  def save_status!(status)
+    if status
+      FileUtils.touch STATUS_FILE
+    else
+      FileUtils.rm STATUS_FILE
     end
   end
 
+  def saved_status
+    File.exits? STATUS_FILE
+  end
 end
