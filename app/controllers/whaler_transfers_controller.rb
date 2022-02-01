4ee2b8973e2a0400a04389aacd3fb97316049f7a
Danil Pismenny <danil@brandymint.ru>
2022-02-02 01:00:39 +0300

    Fix routes

---
+++ app/controllers/whaler_transfers_controller.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+class WhalerTransfersController < ResourcesController
+  layout 'fluid'
+end
--- app/controllers/whaller_transfers_controller.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-class WhallerTransfersController < ResourcesController
-  layout 'fluid'
-end
