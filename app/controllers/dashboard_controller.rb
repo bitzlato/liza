40f9628fc781e849f0db2dae010b07a4e6dee2c2
Eugene Stepanov <antynitr@gmail.com>
2022-02-03 12:18:54 +0300

    Take into account bitzlato wallet balances

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -6,6 +6,7 @@ class DashboardController < ResourcesController
   layout 'fluid'
   def index
     render locals: {
+      bitzlato_balances: BitzlatoWallet.market_balances,
       system_balances: Wallet.all.each_with_object({}) { |w, a| w.available_balances.each_pair { |c, b| a[c] ||= 0.0; a[c] += b.to_d } }, # rubocop:disable Style/Semicolon
       accountable_fee: Transaction.accountable_fee.group(:fee_currency_id).sum(:fee),
       adjustments: Adjustment.accepted.group(:currency_id, :category).sum(:amount),
