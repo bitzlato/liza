1b07084eff7afb8ac23b4224c188f4432a960ab4
mailer21 <metelew@gmail.com>
2022-02-16 11:17:03 +0300

    deploy to s1

---
+++ config/deploy/s1.rb
@@ -0,0 +1,17 @@
+# Copyright (c) 2019 Danil Pismenny <danil@brandymint.ru>
+
+# frozen_string_literal: true
+
+set :user, 'app'
+
+set :rails_env, :staging
+set :disallow_pushing, false
+set :application, -> { 'liza-' + fetch(:stage).to_s }
+set :deploy_to, -> { "/home/#{fetch(:user)}/#{fetch(:stage)}/#{fetch(:application)}" }
+set :puma_bind, -> { "unix://#{shared_path}/tmp/sockets/puma.sock" }
+
+server ENV.fetch('STAGING_SERVER'),
+  user: fetch(:user),
+  port: '22',
+  roles: %w[app db].freeze,
+  ssh_options: { forward_agent: true }
