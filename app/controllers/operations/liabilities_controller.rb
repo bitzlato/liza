5857e05a7c90a4e2bc15958463be403a71e0bd46
Eugene Stepanov <antynitr@gmail.com>
2022-04-20 19:39:31 +0300

    [#181833390] Add exclude_bots scope to Operations::Liability

--- app/controllers/operations/liabilities_controller.rb
+++ app/controllers/operations/liabilities_controller.rb
@@ -6,5 +6,11 @@ module Operations
   class LiabilitiesController < ResourcesController
     include CurrencySupport
     layout 'fluid'
+
+    private
+
+    def index_form
+      'operation_form'
+    end
   end
 end
--- app/models/operations/liability.rb
+++ app/models/operations/liability.rb
@@ -6,5 +6,13 @@ module Operations
   # {Liability} is a balance sheet operation
   class Liability < Operation
     belongs_to :member
+
+    scope :exclude_bots, ->(exclude) do
+      where.not(member_id: [Member::DEEP_STONER_BOT_ID, Member::BARGAINER_BOT_ID]) if exclude
+    end
+
+    def self.ransackable_scopes(auth_object = nil)
+      [:exclude_bots]
+    end
   end
 end
