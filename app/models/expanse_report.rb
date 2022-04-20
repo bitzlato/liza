6fe1bf28fb52588e1d794dc2524c2046aef8528c
Eugene Stepanov <antynitr@gmail.com>
2022-04-20 14:35:37 +0300

    [#181595509] Expanse report

---
+++ app/models/expanse_report.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+class ExpanseReport < Report
+  def results
+    super.merge(
+      records: reporter.records
+    )
+  end
+
+  class Generator < BaseGenerator
+    def perform
+      records
+    end
+
+    def records
+      transaction_fees.map do |group, fee|
+        { blockchain_name: group[0], currency: group[1], fee: fee }
+      end
+    end
+
+    private
+
+    def q
+      { reference_type_in: %w[Wallet Withdraw], created_at_gt: form.time_from, created_at_lteq: form.time_to }
+    end
+
+    def transaction_fees
+      Transaction.ransack(q).result.joins(:blockchain).group(:'blockchains.name', :fee_currency_id).sum(:fee)
+    end
+  end
+end
