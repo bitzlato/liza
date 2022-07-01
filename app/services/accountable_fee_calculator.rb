944ddac2961b57d29be84810c491d25f413f7c62
Aleksandr B. Ivanov <aleksandr.i@bitzlato.com>
2022-07-01 17:36:39 +0300

    [#182285985] Belomor solana

--- app/services/accountable_fee_calculator.rb
+++ app/services/accountable_fee_calculator.rb
@@ -4,19 +4,10 @@ class AccountableFeeCalculator
   PEATIO_CLIENT_APPLICATION_ID = 2
 
   def call
-    belomor_fees = BelomorBlockchainTransaction
-                   .joins(:blockchain)
-                   .accountable_fee
-                   .where(client_application_id: PEATIO_CLIENT_APPLICATION_ID)
-                   .where.not(blockchains: { key: 'solana-mainnet' })
-                   .group(:fee_currency_id)
-                   .sum(:fee)
-    solana_fees = Transaction
-                  .joins(:blockchain)
-                  .accountable_fee
-                  .where(blockchains: { key: 'solana-mainnet' })
-                  .group(:fee_currency_id)
-                  .sum(:fee)
-    belomor_fees.merge(solana_fees) { |_key, old_value, new_value| old_value + new_value }
+    BelomorBlockchainTransaction
+      .accountable_fee
+      .where(client_application_id: PEATIO_CLIENT_APPLICATION_ID)
+      .group(:fee_currency_id)
+      .sum(:fee)
   end
 end
