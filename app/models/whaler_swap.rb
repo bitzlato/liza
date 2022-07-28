a14565c56b1f7f7cfb1e7455c8147b65019e8387
Eugene Stepanov <antynitr@gmail.com>
2022-07-28 11:19:14 +0300

    fix swap counting

--- app/models/whaler_swap.rb
+++ app/models/whaler_swap.rb
@@ -1,8 +1,10 @@
 class WhalerSwap < WhalerRecord
   self.table_name = :swaps
 
+  scope :done, -> { where(state: 'done') }
+
   def self.wallet_total_amount_for(cc_code)
-    where(from_currency_code: cc_code).sum('from_amount + fee') -
-      where(to_currency_code: cc_code).sum('to_amount')
+    done.where(from_currency_code: cc_code).sum('from_amount + fee') -
+      done.where(to_currency_code: cc_code).sum('to_amount')
   end
 end
