d7875b34833117f771b4ae93e3f6413c767c9e83
Aleksandr B. Ivanov <aleksandr.i@bitzlato.com>
2022-08-17 15:57:12 +0300

    Fix deposit balances

--- app/services/address_balances_query.rb
+++ app/services/address_balances_query.rb
@@ -4,7 +4,7 @@ class AddressBalancesQuery
   def deposit_balances
     BelomorDepositAddress
       .connection
-      .execute('SELECT "key", sum("val"::decimal) FROM deposit_addresses, LATERAL jsonb_each_text(balances) AS each(KEY,val) WHERE kind = 0 AND client_application_id = 2 GROUP BY "key"')
+      .execute('SELECT "key", sum("val"::decimal) FROM deposit_addresses, LATERAL jsonb_each_text(balances) AS each(KEY,val) WHERE client_application_id = 2 GROUP BY "key"')
       .each_with_object({}) { |r, a| a[r['key'].downcase] = r['sum'] }
   end
 
