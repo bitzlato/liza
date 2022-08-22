e5d21182b2c366c4f44ae716ffac275b675cf626
Eugene Stepanov <antynitr@gmail.com>
2022-08-22 12:05:57 +0300

    fix summary report

--- app/helpers/rate_helper.rb
+++ app/helpers/rate_helper.rb
@@ -4,13 +4,13 @@ module RateHelper
   end
 
   def current_rates(default_rate = 'USD')
-    current_rates = whaler_client.rates(default_rate).yield_self do |data|
+    @current_rates ||= whaler_client.rates(default_rate).yield_self do |data|
       data['rates'].transform_keys!(&:downcase)
       data
     end
   end
 
   def get_rate_for(currency_id)
-    current_rates[currency_id]
+    current_rates['rates'][currency_id]
   end
 end
