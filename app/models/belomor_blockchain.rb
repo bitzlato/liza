618e990472506ed7ae67a2c963e4052b8ce84374
Aleksandr B. Ivanov <aleksandr.i@bitzlato.com>
2022-11-22 16:11:05 +0300

    [#183850531] Add belomor balances

--- app/models/belomor_blockchain.rb
+++ app/models/belomor_blockchain.rb
@@ -1,8 +1,14 @@
 # frozen_string_literal: true
 
 class BelomorBlockchain < BelomorRecord
+  BTC_KEY = 'btc'
+
   self.table_name = 'blockchains'
 
   has_many :blockchain_currencies, class_name: 'BelomorBlockchainCurrency', foreign_key: 'blockchain_id', dependent: :destroy
   has_many :currencies, class_name: 'BelomorCurrency', through: :blockchain_currencies
+
+  def balances
+    key == BTC_KEY ? { 'btc' => balance } : nil
+  end
 end
--- app/services/accountable_fee_calculator.rb
+++ app/services/accountable_fee_calculator.rb
@@ -4,11 +4,12 @@ class AccountableFeeCalculator
   PEATIO_CLIENT_APPLICATION_ID = 2
 
   def call(where = nil)
-    scope = BelomorBlockchainTransaction
-      .accountable_fee
-      .where(client_application_id: PEATIO_CLIENT_APPLICATION_ID)
-      .where(where)
-      .group(:fee_currency_id)
-      .sum(:fee)
+    subquery = BelomorBlockchainTransaction
+               .accountable_fee
+               .where(client_application_id: PEATIO_CLIENT_APPLICATION_ID)
+               .where(where)
+               .select('DISTINCT ON (txid) txid, *')
+               .to_sql
+    BelomorBlockchainTransaction.connection.exec_query("SELECT fee_currency_id, SUM(fee) FROM (#{subquery}) bt GROUP BY fee_currency_id;").rows.to_h
   end
 end
