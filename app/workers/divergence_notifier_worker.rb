37b06d52ea341afd3207758842da705d1ddb5d0a
Aleksandr B. Ivanov <aleksandr.i@bitzlato.com>
2022-11-25 11:32:29 +0300

    [#183850531] Fix DivergenceNotifierWorker

--- app/workers/divergence_notifier_worker.rb
+++ app/workers/divergence_notifier_worker.rb
@@ -105,7 +105,8 @@ class DivergenceNotifierWorker
       bitzlato_balances: BitzlatoWallet.market_balances,
       system_balances: AddressBalancesQuery.new.peatio_wallet_balances,
       accountable_fee: AccountableFeeCalculator.new.call,
-      adjustments: Adjustment.accepted.group(:currency_id, :category).sum(:amount)
+      adjustments: Adjustment.accepted.group(:currency_id, :category).sum(:amount),
+      belomor_balances: BelomorBlockchain.all.map(&:balances).inject { |memo, el| memo.to_h.merge(el.to_h) { |_, x, y| x.to_d + y.to_d } }
     }
   end
 end
